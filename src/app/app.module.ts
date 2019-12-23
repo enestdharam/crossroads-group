6df6dcb4833be07b09d066d5408948f78e07c803
PC-40 <PC-40@PC-40>
2019-12-23 13:53:57 +0530

    Architecture Setup Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthGuard } from './services';
+import { ComponentModule } from './components/components.module';
+import { HttpClientModule } from '@angular/common/http'; 
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    ComponentModule,
+    HttpClientModule
+  ],
+  providers: [AuthGuard  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
