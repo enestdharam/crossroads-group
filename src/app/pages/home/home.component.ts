c6fa8cbff0827eb73e349c50778d7c47c0986543
PC-40 <PC-40@PC-40>
2019-12-23 14:07:59 +0530

    code refactoring

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { CommitsService } from "src/app/services/commits.service";
-import { ActivatedRoute } from "@angular/router";
-import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-home",
@@ -9,25 +7,28 @@ import { Subscription } from "rxjs";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  dataSource : any;
-  displayedColumns: string[] = ['Commiter Name', 'Commiter Email', 'Message', 'Commit Date'];
+  dataSource: any;
+  displayedColumns: string[] = [
+    "Commiter Name",
+    "Commiter Email",
+    "Message",
+    "Commit Date"
+  ];
   loading = true;
-  constructor(private commitService: CommitsService ) {}
+  constructor(private commitService: CommitsService) {}
 
   ngOnInit() {
     this.getRepoCommitHistory();
   }
 
   private getRepoCommitHistory() {
-    this.commitService.commitHistory$.subscribe(response =>{    
-      this.loading  = false;
+    this.commitService.commitHistory$.subscribe(response => {
+      this.loading = false;
       this.dataSource = response;
-      
     });
   }
 
-  refreshCommits(){  
+  refreshCommits() {
     this.loading = true;
     this.commitService.refreshCommits();
   }
--- src/app/services/commits.service.ts
+++ src/app/services/commits.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { GIT_API_URL, OWNER_NAME, REPO_NAME } from "../app.constants";
 import { BehaviorSubject } from "rxjs";
-import { switchMap, map, shareReplay } from "rxjs/operators";
+import { switchMap, shareReplay } from "rxjs/operators";
 @Injectable({ providedIn: "root" })
 export class CommitsService {
   private getCommitsHistory$ = new BehaviorSubject<boolean>(false);
 
   commitHistory$ = this.getCommitsHistory$.pipe(
     switchMap(() => {
-      return this.http.get(GIT_API_URL + OWNER_NAME + "/" + REPO_NAME + "/commits");
+      return this.http.get(`${GIT_API_URL}${OWNER_NAME}/${REPO_NAME}/commits`);
     }),
     shareReplay(1)
   );
@@ -19,6 +19,4 @@ export class CommitsService {
   refreshCommits() {
     this.getCommitsHistory$.next(true);
   }
-
- 
 }
